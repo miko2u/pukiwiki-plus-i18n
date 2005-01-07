8e19a77b1755b99f3ba27cbf3ddfbeeb2a089a51
miko2u <miko@cafelounge.net>
2005-01-07 01:50:46 +0000

    merge official cvs(2004-01-03) #5

--- plugin/search.inc.php
+++ plugin/search.inc.php
@@ -1,46 +1,50 @@
 <?php
-/////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
+// $Id: search.inc.php,v 1.4.1 2005/01/02 05:47:52 miko Exp $
 //
-// $Id: search.inc.php,v 1.2.1 2004/07/31 03:09:20 miko Exp $
-//
-// ¸¡º÷
+// Search plugin
+
+define('PLUGIN_SEARCH_MAX_LENGTH', 80);
+
 function plugin_search_action()
 {
-	global $script,$vars;
-	global $_title_result,$_title_search,$_msg_searching,$_btn_and,$_btn_or,$_btn_search;
+	global $script, $post, $vars, $_title_result, $_title_search;
+	global $_msg_searching, $_btn_and, $_btn_or, $_btn_search;
 
-	$s_word = array_key_exists('word',$vars) ? htmlspecialchars($vars['word']) : '';
-	$type = array_key_exists('type',$vars) ? $vars['type'] : '';
-
-	if ($s_word != '')
-	{
-		$msg = str_replace('$1',$s_word,$_title_result);
-		$body = do_search($vars['word'],$type);
+	$s_word = isset($post['word']) ? htmlspecialchars($post['word']) : '';
+	if (strlen($s_word) > PLUGIN_SEARCH_MAX_LENGTH) {
+		unset($vars['word']);
+		die_message('Search words too long');
 	}
-	else
-	{
-		$msg = $_title_search;
-		$body = "<br />\n$_msg_searching\n";
+
+	$type   = isset($vars['type']) ? $vars['type'] : '';
+
+	if ($s_word == '') {
+		$msg  = $_title_search;
+		$body = '<br />' . "\n" . $_msg_searching . "\n";
+	} else {
+		$msg  = str_replace('$1', $s_word, $_title_result);
+		$body = do_search($vars['word'], $type);
 	}
 
 	$and_check = $or_check = '';
-	if ($type == 'OR')
-		$or_check = ' checked="checked"';
-	else
+	if ($type == 'OR') {
+		$or_check  = ' checked="checked"';
+	} else {
 		$and_check = ' checked="checked"';
+	}
 
 	$body .= <<<EOD
 <form action="$script?cmd=search" method="get">
  <div>
   <input type="hidden" name="cmd" value="search" />
-  <input type="text" name="word" size="20" value="$s_word" />
+  <input type="text"  name="word" value="$s_word" size="20" />
   <input type="radio" name="type" value="AND" $and_check />$_btn_and
-  <input type="radio" name="type" value="OR" $or_check />$_btn_or
+  <input type="radio" name="type" value="OR"  $or_check  />$_btn_or
   &nbsp;<input type="submit" value="$_btn_search" />
  </div>
 </form>
 EOD;
 
-	return array('msg'=>$msg,'body'=>$body);
+	return array('msg'=>$msg, 'body'=>$body);
 }
