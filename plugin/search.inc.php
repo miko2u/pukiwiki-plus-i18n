08858e7b6d1e52655dcfec4f5c113334bcc58e9c
miko2u <miko@cafelounge.net>
2005-02-13 10:03:47 +0000

    merge official(1.4.5)

--- plugin/search.inc.php
+++ plugin/search.inc.php
@@ -1,6 +1,6 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone.
-// $Id: search.inc.php,v 1.5.1 2005/01/09 03:13:41 miko Exp $
+// $Id: search.inc.php,v 1.6.1 2005/01/16 14:10:46 miko Exp $
 //
 // Search plugin
 
@@ -10,18 +10,24 @@ define('PLUGIN_SEARCH_DISABLE_GET_ACCESS', 0); // 1, 0
 
 define('PLUGIN_SEARCH_MAX_LENGTH', 80);
 
+// Show a search box on a page
+function plugin_search_convert()
+{
+	static $done;
+
+	if (func_get_args()) {
+		return '#search(): No argument<br/>' . "\n";
+	} else if (isset($done)) {
+		return '#search(): You already view a search box<br/>' . "\n";
+	} else {
+		$done = TRUE;
+		return plugin_search_search_form();
+	}
+}
+
 function plugin_search_action()
 {
-	global $script, $post, $vars, $_title_result, $_title_search;
-	global $_msg_searching, $_btn_and, $_btn_or, $_btn_search;
-
-	$type   = isset($vars['type']) ? $vars['type'] : '';
-	$and_check = $or_check = '';
-	if ($type == 'OR') {
-		$or_check  = ' checked="checked"';
-	} else {
-		$and_check = ' checked="checked"';
-	}
+	global $post, $vars, $_title_result, $_title_search, $_msg_searching;
 
 	if (PLUGIN_SEARCH_DISABLE_GET_ACCESS) {
 		$s_word = isset($post['word']) ? htmlspecialchars($post['word']) : '';
@@ -32,28 +38,39 @@ function plugin_search_action()
 		unset($vars['word']); // Stop using $_msg_word at lib/html.php
 		die_message('Search words too long');
 	}
-	if ($s_word == '') {
+
+	$type = isset($vars['type']) ? $vars['type'] : '';
+
+	if ($s_word != '') {
+		// Search
+		$msg  = str_replace('$1', $s_word, $_title_result);
+		$body = do_search($vars['word'], $type);
+	} else {
+		// Init
 		unset($vars['word']); // Stop using $_msg_word at lib/html.php
 		$msg  = $_title_search;
 		$body = '<br />' . "\n" . $_msg_searching . "\n";
+	}
+
+	// Show search form
+	$body .= plugin_search_search_form($s_word, $type);
+
+	return array('msg'=>$msg, 'body'=>$body);
+}
+
+function plugin_search_search_form($s_word = '', $type = '')
+{
+	global $script, $_btn_and, $_btn_or, $_btn_search;
+
+	$and_check = $or_check = '';
+	if ($type == 'OR') {
+		$or_check  = ' checked="checked"';
 	} else {
-		$msg  = str_replace('$1', $s_word, $_title_result);
-		$body = do_search($vars['word'], $type);
+		$and_check = ' checked="checked"';
 	}
 
 	if (PLUGIN_SEARCH_DISABLE_GET_ACCESS) {
-	$body .= <<<EOD
-<form action="$script?cmd=search" method="post">
- <div>
-  <input type="text"  name="word" value="$s_word" size="20" />
-  <input type="radio" name="type" value="AND" $and_check />$_btn_and
-  <input type="radio" name="type" value="OR"  $or_check  />$_btn_or
-  &nbsp;<input type="submit" value="$_btn_search" />
- </div>
-</form>
-EOD;
-	} else {
-	$body .= <<<EOD
+	return <<<EOD
 <form action="$script" method="get">
  <div>
   <input type="hidden" name="cmd" value="search" />
@@ -65,6 +82,15 @@ EOD;
 </form>
 EOD;
 	}
-
-	return array('msg'=>$msg, 'body'=>$body);
+	return <<<EOD
+<form action="$script?cmd=search" method="post">
+ <div>
+  <input type="text"  name="word" value="$s_word" size="20" />
+  <input type="radio" name="type" value="AND" $and_check />$_btn_and
+  <input type="radio" name="type" value="OR"  $or_check  />$_btn_or
+  &nbsp;<input type="submit" value="$_btn_search" />
+ </div>
+</form>
+EOD;
 }
+?>
