f2b037af747934a034a2f7e5b33de5b73971e614
miko2u <miko@cafelounge.net>
2005-03-09 05:46:25 +0000

    Test for gettext messages #3

--- po/makelang.sh
+++ po/makelang.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-msgfmt -o pukiwiki.mo pukiwiki.po add.po article.po back.po memo.po source.po vote.po versionlist.po yetlist.po
+msgfmt -o pukiwiki.mo pukiwiki.ja.po add.ja.po article.ja.po back.ja.po memo.ja.po source.ja.po vote.ja.po versionlist.ja.po yetlist.ja.po
 cp pukiwiki.mo ../locale/ja/LC_MESSAGES
