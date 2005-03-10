08fb85ff0f7cd9caefd7fe4ca746323664e5fbf4
miko2u <miko@cafelounge.net>
2005-03-10 08:05:31 +0000

    additional plugin-original gettext #10

--- locale/pot/makelang.sh
+++ locale/pot/makelang.sh
@@ -1,24 +1,43 @@
 #!/bin/sh
 
-LOCALE=../locale
+LOCALE=..
 LANG=ja
 
 msgfmt -o $LOCALE/$LANG/LC_MESSAGES/pukiwiki.mo pukiwiki.po
-msgfmt -o $LOCALE/$LANG/LC_MESSAGES/add.mo add.ja.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/add.mo add.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/addline.mo addline.po
 msgfmt -o $LOCALE/$LANG/LC_MESSAGES/article.mo article.po
-msgfmt -o $LOCALE/$LANG/LC_MESSAGES/back.mo back.ja.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/attach.mo attach.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/back.mo back.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/backup.mo backup.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/calendar2.mo calendar2.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/calendar_viewer.mo calendar_viewer.po
 msgfmt -o $LOCALE/$LANG/LC_MESSAGES/comment.mo comment.po
 msgfmt -o $LOCALE/$LANG/LC_MESSAGES/deleted.mo deleted.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/diff.mo diff.po
 msgfmt -o $LOCALE/$LANG/LC_MESSAGES/freeze.mo freeze.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/include.mo include.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/insert.mo insert.po
 msgfmt -o $LOCALE/$LANG/LC_MESSAGES/interwiki.mo interwiki.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/list.mo list.po
 msgfmt -o $LOCALE/$LANG/LC_MESSAGES/ls2.mo ls2.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/memo.mo memo.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/minicalendar.mo minicalendar.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/minicalendar_viewer.mo minicalendar_viewer.po
 msgfmt -o $LOCALE/$LANG/LC_MESSAGES/navi.mo navi.po
 msgfmt -o $LOCALE/$LANG/LC_MESSAGES/newpage.mo newpage.po
-msgfmt -o $LOCALE/$LANG/LC_MESSAGES/memo.mo memo.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/paint.mo paint.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/pcomment.mo pcomment.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/popular.mo popular.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/recent.mo recent.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/referer.mo referer.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/rename.mo rename.po
 msgfmt -o $LOCALE/$LANG/LC_MESSAGES/search.mo search.po
 msgfmt -o $LOCALE/$LANG/LC_MESSAGES/source.mo source.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/template.mo template.po
 msgfmt -o $LOCALE/$LANG/LC_MESSAGES/tracker.mo tracker.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/transit.mo transit.po
 msgfmt -o $LOCALE/$LANG/LC_MESSAGES/unfreeze.mo unfreeze.po
-msgfmt -o $LOCALE/$LANG/LC_MESSAGES/vote.mo vote.po
 msgfmt -o $LOCALE/$LANG/LC_MESSAGES/versionlist.mo versionlist.po
+msgfmt -o $LOCALE/$LANG/LC_MESSAGES/vote.mo vote.po
 msgfmt -o $LOCALE/$LANG/LC_MESSAGES/yetlist.mo yetlist.po
