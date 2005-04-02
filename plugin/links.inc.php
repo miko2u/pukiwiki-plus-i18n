d0fe00f4c1ef5a93199d708c8df2101407465cf8
miko2u <miko@cafelounge.net>
2005-04-02 06:15:33 +0000

    Add links.pot

--- plugin/links.inc.php
+++ plugin/links.inc.php
@@ -9,22 +9,19 @@ function plugin_links_init()
 {
 	$messages = array(
 		'_links_messages'=>array(
-			'title_update'  => 'キャッシュ更新',
-			'msg_adminpass' => '管理者パスワード',
-			'btn_submit'    => '実行',
-			'msg_done'      => 'キャッシュの更新が完了しました。',
-			'msg_usage'     => "
-* 処理内容
-
-:キャッシュを更新|
-全てのページをスキャンし、あるページがどのページからリンクされているかを調査して、キャッシュに記録します。
-
-* 注意
-実行には数分かかる場合もあります。実行ボタンを押したあと、しばらくお待ちください。
-
-* 実行
-管理者パスワードを入力して、[実行]ボタンをクリックしてください。
-"
+			'title_update'  => _("Cash update"),
+			'msg_adminpass' => _("Administrator password"),
+			'btn_submit'    => _("Exec"),
+			'msg_done'      => _("The update of cash was completed."),
+			'msg_usage'     => 
+_("* Content of processing\n") .
+_(":Cash update|\n") .
+_("All pages are scanned, whether on which page certain pages have been linked is investigated, and it records in the cache.\n\n") .
+_("* CAUTION\n") .
+_("It is likely to drive it for a few minutes in execution.") .
+_("Please wait for a while after pushing the execution button.\n\n") .
+_("* EXEC\n") .
+_("Please input the Administrator password, and click the [Exec] button.\n")
 		)
 	);
 	set_plugin_messages($messages);
@@ -40,7 +37,7 @@ function plugin_links_action()
 	$msg = $body = '';
 	if (empty($vars['action']) || empty($post['adminpass']) || ! pkwk_login($post['adminpass'])) {
 		$msg   = & $_links_messages['title_update'];
-		$body  = convert_html($_links_messages['msg_usage']);
+		$body  = convert_html( sprintf($_links_messages['msg_usage']) );
 		$body .= <<<EOD
 <form method="POST" action="$script">
  <div>
