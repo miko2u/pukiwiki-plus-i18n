afb0093b52eb6841b2d920aa036160acbc4474c8
miko2u <miko@cafelounge.net>
2005-04-04 05:50:56 +0000

    fix lib/init.php

--- lib/init.php
+++ lib/init.php
@@ -57,7 +57,6 @@ set_language();
 set_time();
 require(LIB_DIR . 'public_holiday.php');
 
-/////////////////////////////////////////////////
 // Init Resource(for gettext)
 putenv('LC_ALL=' . PO_LANG);
 setlocale(LC_ALL, PO_LANG);
@@ -71,31 +70,6 @@ require(LIB_DIR . 'resource.php');
 // Init encoding hint
 define('PKWK_ENCODING_HINT', isset($_LANG['encode_hint']) ? $_LANG['encode_hint'] : '');
 // unset($_LANG['encode_hint']);
-
-/////////////////////////////////////////////////
-// INI_FILE: Require LANG_FILE
-//
-//if(defined('LANG_ENCODING') && LANG_ENCODING != '') {
-//	define('LANG_FILE_HINT',    LANG_DIR . LANG . '.' . LANG_ENCODING . '.lng.php');
-//	define('LANG_FILE',         LANG_DIR . UI_LANG . '.' . LANG_ENCODING . '.lng.php');    // For UI resource
-//} else {
-//	define('LANG_FILE_HINT',    LANG_DIR . LANG . '.lng.php');     // For encoding hint
-//	define('LANG_FILE',         LANG_DIR . UI_LANG . '.lng.php');  // For UI resource
-//}
-//$die = '';
-//foreach (array('LANG_FILE_HINT', 'LANG_FILE') as $langfile) {
-//	if (! file_exists(constant($langfile)) || ! is_readable(constant($langfile))) {
-//		$die .= 'File is not found or not readable. (' . $langfile . ')' . "\n";
-//	} else {
-//		require_once(constant($langfile));
-//	}
-//}
-//if ($die) die_message(nl2br("\n\n" . $die));
-
-/////////////////////////////////////////////////
-// LANG_FILE: Init severn days of the week
-//
-//$weeklabels = $_string['week'];
 
 /////////////////////////////////////////////////
 // INI_FILE: Init $script
