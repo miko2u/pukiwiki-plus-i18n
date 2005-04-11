c56db197858cf2264f7f64e29ee98b6f59ede299
miko2u <miko@cafelounge.net>
2005-04-11 03:59:13 +0000

    fix init.php (use array_merge) #2

--- lib/init.php
+++ lib/init.php
@@ -344,9 +344,6 @@ require(DATA_HOME . 'rules.ini.php');
 // 現在時刻
 $now = format_date(UTIME);
 
-// 文字列置換ルールの初期化
-$line_rules = array();
-
 // 日時置換ルールを$line_rulesに加える
 if ($usedatetime) $line_rules = array_merge($datetime_rules,$line_rules);
 unset($datetime_rules);
