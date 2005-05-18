4183eeaf940d6d5aa12c3822ed6880d1b1569339
miko2u <miko@cafelounge.net>
2005-05-18 06:37:20 +0000

    Fix hide extended-link on "SideBar"

--- plugin/side.inc.php
+++ plugin/side.inc.php
@@ -2,7 +2,7 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: side.inc.php,v 1.6.6 2004/12/15 14:37:37 miko Exp $
+// $Id: side.inc.php,v 1.8.6 2005/05/17 14:37:37 miko Exp $
 //
 
 // サブメニューを使用する
@@ -36,39 +36,45 @@ function plugin_side_convert()
 			return '';
 		}
 
-	}
+	} else {
+		// Output sidebar page data
+		$page = ($side === NULL) ? $sidebar : $side;
 
-	$page = ($side === NULL) ? $sidebar : $side;
-
-	if (SIDE_ENABLE_SUBMENU) {
-		$path = explode('/', strip_bracket($vars['page']));
-		while(count($path)) {
-			$_page = join('/', $path) . '/' . SIDE_SUBMENUBAR;
-			if (is_page($_page)) {
-				$page = $_page;
-				break;
+		if (SIDE_ENABLE_SUBMENU) {
+			$path = explode('/', strip_bracket($vars['page']));
+			while(count($path)) {
+				$_page = join('/', $path) . '/' . SIDE_SUBMENUBAR;
+				if (is_page($_page)) {
+					$page = $_page;
+					break;
+				}
+				array_pop($path);
 			}
-			array_pop($path);
+		}
+
+		if (! is_page($page)) {
+			return '';
+		} else if ($vars['page'] == $page) {
+			return '<!-- #side(): You already view ' . htmlspecialchars($page) . ' -->';
+		} else {
+			// Cut fixed anchors
+			$sidetext = preg_replace('/^(\*{1,3}.*)\[#[A-Za-z][\w-]+\](.*)$/m','$1$2',get_source($page));
+//miko patched
+			if (function_exists('convert_filter')) {
+				$sidetext = convert_filter($sidetext);
+			}
+			global $top, $use_open_uri_in_new_window;
+			$tmptop = $top;
+			$top = '';
+			$save_newwindow = $use_open_uri_in_new_window;
+			$use_open_uri_in_new_window = 0;
+			$sidehtml = convert_html($sidetext);
+			$top = $tmptop;
+			$use_open_uri_in_new_window = $save_newwindow;
+			$sidehtml = str_replace("\n",'',$sidehtml);
+			return preg_replace('/<ul class="list[^>]*>/','<ul class="menu">',$sidehtml);
+//miko patched
 		}
 	}
-
-	if (! is_page($page)) {
-		return '';
-	} else if ($vars['page'] == $page) {
-		return '<!-- #side(): You already view ' .
-			htmlspecialchars($page) . ' -->';
-	}
-
-	$sidetext = preg_replace('/^(\*{1,3}.*)\[#[A-Za-z][\w-]+\](.*)$/m','$1$2',get_source($page));
-	if (function_exists('convert_filter')) {
-		$sidetext = convert_filter($sidetext);
-	}
-	global $top;
-	$tmptop = $top;
-	$top = '';
-	$sidehtml = convert_html($sidetext);
-	$top = $tmptop;
-	$sidehtml = str_replace("\n",'',$sidehtml);
-	return preg_replace('/<ul class="list[^>]*>/','<ul class="menu">',$sidehtml);
 }
 ?>
