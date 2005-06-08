263d38ff2dac94fd6500a7ea89fcc5a51bd4f421
miko2u <miko@cafelounge.net>
2005-06-08 13:56:56 +0000

    It corresponds to do the format specification.

--- plugin/worldtime.inc.php
+++ plugin/worldtime.inc.php
@@ -3,23 +3,29 @@
  * World Time プラグイン
  *
  * @copyright   Copyright &copy; 2005, Katsumi Saito <katsumi@jo1upk.ymt.prug.or.jp>
- * @version     $Id: worldtime.inc.php,v 0.2 2005/06/08 21:32:00 upk Exp $
+ * @version     $Id: worldtime.inc.php,v 0.3 2005/06/08 22:36:00 upk Exp $
  *
  */
 
 function plugin_worldtime_inline()
 {
-	list($code) = func_get_args();
+	switch ( func_num_args() ) {
+	case 1:
+		return "&worldtime( timezone_name ){format};\n";
+	default:
+		list($code,$format) = func_get_args();
+	}
+
 	if (empty($code)) return '';
 
 	$obj = new timezone();
 	$obj->set_datetime();
-	// $obj->set_country($code);
 	$obj->set_tz_name($code);
-
 	list($zone, $zonetime) = $obj->get_zonetime();
-	$x = gmdate('Y-m-d H:i', UTIME + $zonetime);
-	$x .= ' '.$zone;
+
+	if (empty($format)) $format = 'Y-m-d H:i T';
+	$x = gmdate($format, UTIME + $zonetime);
+	$x = str_replace('GMT',$zone,$x);
 	return $x;
 }
 
