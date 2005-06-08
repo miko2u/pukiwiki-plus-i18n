3bbffa2c62ee782a56d0751f4e8815e414036ee8
miko2u <miko@cafelounge.net>
2005-06-08 12:37:50 +0000

    date() is changed to gmdate().

--- plugin/worldtime.inc.php
+++ plugin/worldtime.inc.php
@@ -3,14 +3,14 @@
  * World Time プラグイン
  *
  * @copyright   Copyright &copy; 2005, Katsumi Saito <katsumi@jo1upk.ymt.prug.or.jp>
- * @version     $Id: worldtime.inc.php,v 0.1 2005/03/30 01:00:00 upk Exp $
+ * @version     $Id: worldtime.inc.php,v 0.2 2005/06/08 21:32:00 upk Exp $
  *
  */
 
 function plugin_worldtime_inline()
 {
 	list($code) = func_get_args();
-	if (empty($code)) return;
+	if (empty($code)) return '';
 
 	$obj = new timezone();
 	$obj->set_datetime();
@@ -18,8 +18,8 @@ function plugin_worldtime_inline()
 	$obj->set_tz_name($code);
 
 	list($zone, $zonetime) = $obj->get_zonetime();
-	$x = date("Y-m-d H:i", UTIME+$zonetime);
-	$x .= " ".$zone;
+	$x = gmdate('Y-m-d H:i', UTIME + $zonetime);
+	$x .= ' '.$zone;
 	return $x;
 }
 
