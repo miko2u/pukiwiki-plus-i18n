d7be597e7af87539ab3ddd1b141cfb9520b3d410
miko2u <miko@cafelounge.net>
2005-06-12 15:55:35 +0000

    Addition of linklist and skeylist.

---
+++ plugin/linklist.inc.php
@@ -0,0 +1,246 @@
+<?php
+// $Id: linklist.inc.php,v 0.3 2005/06/07 23:53:00 upk Exp $
+/*
+ * PukiWiki 自動相互リンク作成プラグイン
+ * (C) 2004-2005, Katsumi Saito <katsumi@jo1upk.ymt.prug.or.jp>
+ * License: GPL
+*/
+
+// 構成定義ファイル
+define('CONFIG_REFERER','plugin/referer/config');
+define('CONFIG_LINKLIST','plugin/linklist/config');
+
+function plugin_linklist_init()
+{
+	$msg = array(
+		'_linklist_msg' => array(
+			'h5_title'	=> _('Auto Mutual link'),
+			'title'		=> _('Auto Mutual link of %s'),
+			'not_effective'	=> _('The function of Referer is not effective.'),
+			'no_data'	=> _('no data.'),
+		)
+	);
+	set_plugin_messages($msg);
+}
+
+function plugin_linklist_action()
+{
+	global $vars;
+	global $_linklist_msg;
+	global $referer;
+
+	$retval['msg']  = sprintf($_linklist_msg['title'],$vars['page']);
+	if (! $referer) {
+		$retval['body'] = '<div>'.$_linklist_msg['not_effective']."</div>\n";
+		return $retval;
+	}
+
+	$page = (empty($vars['page'])) ? '' : $vars['page'];
+	$max  = (empty($vars['max']))  ? -1 : $vars['max'];
+
+	$data = tb_get(tb_get_filename($page,'.ref'));
+
+	//  データ無し
+	if (count($data) == 0)
+	{
+		$retval['body'] = '<div>'.$_linklist_msg['no_data']."</div>\n";
+		return $retval;
+	}
+
+	$data = linklist_analysis($data);
+	// 0:検索キー 1:参照カウンタ
+	usort($data,create_function('$a,$b','return $b[1] - $a[1];'));
+	$data = linklist_print($data,$max,0);
+
+	$retval['body']  = '<div>';
+	$retval['body'] .= (empty($data)) ? $_linklist_msg['no_data'] : $data;
+	$retval['body'] .= "</div>\n";
+	return $retval;
+}
+
+function plugin_linklist_convert()
+{
+	global $vars;
+	global $_linklist_msg;
+	global $referer;
+
+	if (! $referer) return;
+
+	list($page,$max) = func_get_args();
+	if (empty($page)) $page = $vars['page'];
+	if (empty($max)) $max = -1;
+
+	$data = tb_get(tb_get_filename($page,'.ref'));
+	if (count($data) == 0) return; //  データ無し
+	$data = linklist_analysis($data);
+	// 0:検索キー 1:参照カウンタ
+	usort($data,create_function('$a,$b','return $b[1] - $a[1];'));
+	$data = linklist_print($data,$max,1);
+	return '<div>'.$data."</div>\n";
+}
+
+// 構成定義ファイル読込
+function linklist_config_load()
+{
+	global $config_linklist;
+
+	// config.php
+	if (!isset($config_linklist))
+	{
+		$config = new Config(CONFIG_LINKLIST);
+		$config->read();
+		$config_linklist['spam'] = $config->get('SPAM');
+		$config_linklist['misc'] = $config->get('MISC');
+		$config_linklist['key']  = $config->get('KEY');
+		unset($config);
+	}
+
+}
+
+// データを解析
+function linklist_analysis($data)
+{
+	global $script;
+	global $config_linklist;
+
+	// 構成定義ファイル読込
+	linklist_config_load();
+	$IgnoreHost = array_merge($config_linklist['spam'], $config_linklist['misc']);
+
+	$rc = array();
+	$i = 0;
+
+	// 自サイトの特定
+	$my = parse_url($script);
+	$my = $my['host'];
+
+	// 0:最終更新日時 1:初回登録日時 2:参照カウンタ 3:Referer ヘッダ 4:利用可否フラグ(1は有効)
+	foreach ($data as $x)
+	{
+		if ($x[4] != 1) continue;
+		// 'scheme', 'host', 'port', 'user', 'pass', 'path', 'query', 'fragment'
+		$url = parse_url($x[3]);
+		if (empty($url['host'])) continue;
+		if (strpos($url['host'],'.') == '') continue; // ホスト名にピリオドが１つもない
+		if (linklist_ignore_check($url['host'])) continue;
+
+		$sw = 0;
+		foreach ($IgnoreHost as $y) {
+			if (strpos($url['host'],$y) !== FALSE) {
+				$sw = 1;
+				continue;
+			}
+		}
+		if ($sw) continue;
+
+		if (strpos($url['host'],$my) !== FALSE) continue;
+
+		$sw = 0;
+		// queryストリングの解析
+		$tok = strtok($url['query'],'&');
+		while($tok) {
+			list($key,$parm)= split ('=', $tok); // キーと値に分割
+			$tok = strtok('&'); // 次の処理の準備
+
+			// 検索キーかの判定
+			$skey = '';
+
+			foreach ($config_linklist['key'] as $y)
+			{
+				if ( (strpos($key,$y) === 0 )) {
+					$skey = $y;
+					continue;
+				}
+			}
+			if ($skey !== $key) continue;
+			if (empty($parm)) continue; // 値が入っていない場合
+
+			// 検索エンジンからきたもの
+			$sw = 1;
+			break;
+		}
+
+		// 検索エンジン以外 かつ 架空ホスト以外 の場合は蓄積
+		// if (!$sw and linklist_testipaddress($url['host']) )
+		// 検索エンジン以外の場合は蓄積
+		if (!$sw)
+		{
+			$rc[$i][0] = $x[3];	// 3:Referer ヘッダ
+			$rc[$i][1] = $x[2];	// 参照カウンタ
+			$i++;
+		}
+	}
+	return $rc;
+}
+
+// データを加工
+function linklist_print($data,$max,$title)
+{
+	global $_linklist_msg;
+
+	// 無制限は、-1 のために判断
+	if ($max > 0)
+	{
+		$data = array_splice($data,0,$max);
+	}
+	$i = count($data);
+	if ($i == 0) return;
+
+	$rc = '';
+	if ($title)
+	{
+		$rc .= '<h5>'.$_linklist_msg['h5_title'].' ';
+		$rc .= ($max > 0) ? $max : $i;
+		$rc .= "</h5>\n";
+	}
+
+	$rc .= "<ul>\n";
+	foreach ($data as $x)
+	{
+		$str = rawurldecode($x[0]);
+		$str = mb_convert_encoding($str,SOURCE_ENCODING,'auto');
+		$tmp = '<a href="'.$x[0].'">'.$str.'</a>('.$x[1].')';
+		$rc .= '<li>'.$tmp."</li>\n";
+	}
+	$rc .= "</ul>\n";
+	return $rc;
+}
+
+function linklist_ignore_check($url)
+{
+	static $ignore_url;
+
+	// config.php
+	if (!isset($ignore_url))
+	{
+		$config = new Config(CONFIG_REFERER);
+		$config->read();
+		$ignore_url = $config->get('IGNORE');
+		unset($config);
+	}
+
+	foreach ($ignore_url as $x)
+	{
+		if (strpos($url,$x) !== FALSE)
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+
+// ホスト名からIPアドレスに変換して評価する
+function linklist_testipaddress ($host)
+{
+	$ip = gethostbyname($host); // ホスト名からIPアドレスを得る
+	if ($ip == $host)
+	{
+		// そもそも IPアドレスが指定されている場合の考慮
+		$name = @gethostbyaddr($host);	// IPアドレスからホスト名を得る
+		if (!empty($name)) return 1;	// lookup できた
+		return 0; // 変換不能
+	}
+	return 1; // IP アドレス変換できた
+}
+
+?>
---
+++ plugin/skeylist.inc.php
@@ -0,0 +1,226 @@
+<?php
+// $Id: skeylist.inc.php,v 0.8 2005/05/20 01:01:00 upk Exp $
+/*
+ * PukiWiki 人気検索キープラグイン
+ * (C) 2004, Katsumi Saito <katsumi@jo1upk.ymt.prug.or.jp>
+ * License: GPL
+*/
+
+// 検索エンジン
+// Google
+define('SKEYLIST_SEARCH_URL','http://www.google.com/search?ie=utf8&amp;oe=utf8&amp;q=');
+// Yahoo!
+// define('SKEYLIST_SEARCH_URL','http://search.yahoo.com/search?ei=UTF-8&p=');
+
+function plugin_skeylist_init()
+{
+	$msg = array(
+		'_skeylist_msg' => array(
+			'h5_title'	=> _('Search Key Top %d'),
+			'title'		=> _('All the Search Key of %s'),
+			'not_effective'	=> _('The function of Referer is not effective.'),
+			'no_data'	=> _('no data.'),
+		)
+	);
+	set_plugin_messages($msg);
+}
+
+function plugin_skeylist_action()
+{
+	global $vars;
+	global $_skeylist_msg;
+	global $referer;
+
+	$retval['msg']  = sprintf($_skeylist_msg['title'],$vars['page']);
+	if (! $referer) {
+		$retval['body'] = '<div>'.$_skeylist_msg['not_effective']."</div>\n";
+		return $retval;
+	}
+
+	$page = (empty($vars['page'])) ? '' : $vars['page'];
+	$max  = (empty($vars['max']))  ? -1 : $vars['max'];
+
+	$data = tb_get(tb_get_filename($page,'.ref'));
+
+	//  データ無し
+	if (count($data) == 0)
+	{
+		$retval['body'] = '<div>'.$_skeylist_msg['no_data']."</div>\n";
+		return $retval;
+	}
+
+	$data = skeylist_analysis($data);
+	// 0:検索キー 1:参照カウンタ
+	usort($data,create_function('$a,$b','return $b[1] - $a[1];'));
+	$data = skeylist_print($data,$max);
+
+	$retval['body']  = '<div>';
+	$retval['body'] .= (empty($data)) ? $_skeylist_msg['no_data'] : $data;
+	$retval['body'] .= "<div>\n";
+	return $retval;
+}
+
+function plugin_skeylist_convert()
+{
+	global $vars;
+	global $_skeylist_msg;
+	global $referer;
+
+	if (! $referer) return '';
+
+	list($page,$max) = func_get_args();
+	if (empty($page)) $page = $vars['page'];
+	if (empty($max)) $max = 10;
+
+	$data = tb_get(tb_get_filename($page,'.ref'));
+	if (count($data) == 0) return ''; //  データ無し
+	$data = skeylist_analysis($data);
+	// 0:検索キー 1:参照カウンタ
+	usort($data,create_function('$a,$b','return $b[1] - $a[1];'));
+	$data = skeylist_print($data,$max);
+	return '<div>'.$data."</div>\n";
+}
+
+
+
+// データを解析
+function skeylist_analysis($data)
+{
+	// 検索エンジンがキーワードとして使うキー
+	$WordsToExtractSearchUrl = array(
+		'q',		// google, msn, earthlink
+		'p',		// yahoo
+		'MT',		// goo
+		'kw',		// fresheye
+		'query',	// aol
+		'search',	// excite
+		'qt',		// infoseek, looksmart
+		'Text',		// nifty
+		'QueryString',	// www.looksmart.co.jp
+		'key',		// findarticles
+		'qkw',		// nbci
+		'su',		// web.de
+		'qry',		// www.mirago.co.uk
+		'w',		// seznam.cz
+		'name',		// JWord
+	);
+
+	$sum = array();
+
+	// 0:最終更新日時 1:初回登録日時 2:参照カウンタ 3:Referer ヘッダ 4:利用可否フラグ(1は有効)
+	foreach ($data as $x)
+	{
+		if ($x[4] != 1) continue;
+		// 'scheme', 'host', 'port', 'user', 'pass', 'path', 'query', 'fragment'
+		$url = parse_url($x[3]);
+		if (empty($url['query'])) continue; // queryストリングが空の場合は対象外
+
+		// queryストリングの解析
+		$tok = strtok($url['query'],'&');
+		while($tok) {
+			list($key,$parm)= split('=', $tok); // キーと値に分割
+			$tok = strtok('&'); // 次の処理の準備
+
+			// 検索キーかの判定
+			$skey = '';
+			foreach ($WordsToExtractSearchUrl as $y)
+			{
+				if ( (strpos($key,$y) === 0 )) {
+					$skey = $y;
+					continue;
+				}
+			}
+			if ($skey !== $key) continue;
+
+			if (empty($parm)) continue; // 値が入っていない場合
+			if ( (strpos($parm,'cache:') === 0 )) continue; // google のキャッシュなどの場合
+
+			$parm = skeylist_convert_key($parm); // 検索キーを名寄せする
+			$sum[$parm] += $x[2]; // 参照カウンタ
+			break;
+		}
+	}
+
+	$rc = array();
+	$i = 0;
+	foreach ($sum as $key=>$val)
+	{
+		$rc[$i][0] = $key;	// 検索キー
+		$rc[$i][1] = $val;	// 参照カウンタ
+		$i++;
+	}
+	return $rc;
+}
+
+// 検索キーを整形する
+function skeylist_convert_key($x)
+{
+	$rc = '';
+
+	// テーブルに存在する検索エンジンで、指定キーが存在している場合
+	$x = rawurldecode($x);
+	$x = mb_convert_encoding($x,SOURCE_ENCODING,'auto');
+
+	// "K" : 「半角片仮名」を「全角片仮名」に変換
+	// "V" :  濁点付きの文字を一文字に変換
+	// "a" : 「全角」英数字を「半角」に変換
+	// "s" : 「全角」スペースを「半角」に変換
+	$x = mb_convert_kana($x, 'KVas');
+
+	// Yahooなど他のエンジン対応
+	$x = str_replace('+', ' ', $x);		// Yahoo: OR
+	$x = str_replace('#', ' ', $x);		// Yahoo: 否定
+	$x = str_replace('*', ' ', $x);		// Yahoo: AND
+
+	$x = str_replace(' and ', ' ', $x);
+	$x = str_replace(' AND ', ' ', $x);
+	$x = str_replace('"', '', $x);  // "  は除去
+
+	$x = str_replace('|', ' ', $x);
+	$x = str_replace('?', ' ', $x);
+
+	// 文字の途中に入っている連続するスペースを１つにする
+	$tok = strtok($x,' ');
+	while($tok) {
+		$rc .= $tok.' ';
+		$tok = strtok(' ');
+	}
+
+	// 前後のスペースを取り除く
+	$rc = trim($rc);
+	return $rc;
+}
+
+// データを加工
+function skeylist_print($data,$max)
+{
+	global $_skeylist_msg;
+	$rc = '';
+
+	if ($max > 0) {
+		$rc .= '<h5>'.sprintf($_skeylist_msg['h5_title'],$max)."</h5>\n";
+		$rc .= "<ul>\n";
+		$data = array_splice($data,0,$max);
+	}
+
+	foreach ($data as $x)
+	{
+		if ( !strcasecmp('utf-8',SOURCE_ENCODING) ) {
+			$key = $x[0];
+		} else {
+			$key = mb_convert_encoding($x[0],'utf-8',SOURCE_ENCODING);
+		}
+		$key = rawurlencode($key);
+		$tmp = '<a href="'.SKEYLIST_SEARCH_URL.$key.'">'.$x[0].'</a>('.$x[1].')';
+		if ($max > 0) {
+			$rc .= '<li>'.$tmp."</li>\n";
+		} else {
+			$rc .= $tmp."\n";
+		}
+	}
+
+	if ($max > 0) $rc .= "</ul>\n";
+	return $rc;
+}
+
+?>
