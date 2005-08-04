9157c71b951a4775e555a425e352caacbd71a991
miko2u <miko@cafelounge.net>
2005-08-04 08:34:01 +0000

    experimental plugin "expand.inc.php" #2

--- plugin/expand.inc.php
+++ plugin/expand.inc.php
@@ -5,6 +5,8 @@
 // Expand Plugin
 function plugin_expand_action()
 {
+	global $post;
+
 	$lines = preg_replace(array("[\\r|\\n]","[\\r]"), array("\n","\n"), $post['fullcontents']);
 	$lines = preg_replace(array("'<p>'si","'</p>'si"), array("",""), convert_html($lines));
 
@@ -25,9 +27,9 @@ function plugin_expand_convert()
 
 	$lines = preg_replace(array("[\\r|\\n]","[\\r]"), array("\n","\n"), $source);
 	$lines = preg_replace(array("'<p>'si","'</p>'si"), array("",""), convert_html($lines));
-	return '<div style="width:100%;overflow:hidden;">' . $lines . '</div>'
+	return '<div style="width:400px;overflow:hidden;">' . $lines . '</div>'
 		 . '<form method="post" action="' . $script . '"><textarea name="fullcontents" rows="1" cols="1" style="display:none;">'
-		 . htmlspecialchars($source) . '</textarea><input type="submit" name="submit" class="btnExpand" />'
+		 . htmlspecialchars($source) . '</textarea><input type="submit" name="submit" class="btnExpand" value=" " />'
 		 . '<input type="hidden" name="cmd" value="expand" /></form>';
 }
 ?>
