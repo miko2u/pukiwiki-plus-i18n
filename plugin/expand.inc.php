267b1b653c70acfc23da42adc8968e31548c70db
miko2u <miko@cafelounge.net>
2005-08-04 08:59:08 +0000

    experimental plugin "expand.inc.php" #3

--- plugin/expand.inc.php
+++ plugin/expand.inc.php
@@ -18,16 +18,22 @@ function plugin_expand_action()
 function plugin_expand_convert()
 {
 	$numargs = func_num_args();
-	if ($numargs != 1) {
+	if ($numargs == 2) {
+		list($width,$source) = func_get_args();
+		$width = intval($width);
+	} else if ($numargs == 1) {
+		list($source) = func_get_args();
+		$width = 380;
+	} else {
 		return '';
 	}
+	if (!defined($width) || $width < 380){ return ''; }
 
-	list($source) = func_get_args();
 	$script = get_script_uri();
 
 	$lines = preg_replace(array("[\\r|\\n]","[\\r]"), array("\n","\n"), $source);
 	$lines = preg_replace(array("'<p>'si","'</p>'si"), array("",""), convert_html($lines));
-	return '<div style="width:400px;overflow:hidden;">' . $lines . '</div>'
+	return '<div style="width:' . $width . 'px;overflow:hidden;">' . $lines . '</div>'
 		 . '<form method="post" action="' . $script . '"><textarea name="fullcontents" rows="1" cols="1" style="display:none;">'
 		 . htmlspecialchars($source) . '</textarea><input type="submit" name="submit" class="btnExpand" value=" " />'
 		 . '<input type="hidden" name="cmd" value="expand" /></form>';
