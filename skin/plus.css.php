0a903f4af7ff732f6109c0e708f3ea3172d07d4d
miko2u <miko@cafelounge.net>
2005-09-28 06:46:00 +0000

    cleanup

--- skin/plus.css.php
+++ skin/plus.css.php
@@ -4,34 +4,42 @@
 //
 // Default CSS Adaptive Kit
 
-// Send header
-header('Content-Type: text/css');
-$matches = array();
-if(ini_get('zlib.output_compression') && preg_match('/\b(gzip|deflate)\b/i', $_SERVER['HTTP_ACCEPT_ENCODING'], $matches)) {
-	header('Content-Encoding: ' . $matches[1]);
-	header('Vary: Accept-Encoding');
-}
+$css = array();
+$lastmodtime = 0;
 
+$css[] = file_get_contents('pukiwiki.css');
+$lastmodtime = max($lastmodtime, filemtime('pukiwiki.css'));
+$css[] = file_get_contents('plus.css');
+$lastmodtime = max($lastmodtime, filemtime('plus.css'));
+
+$dir = @opendir('./css/');
+while ($file = @readdir($dir)) {
+	$pathparts = pathinfo($file);
+	if (!strcasecmp($path_parts['extension'], 'css')) {
+		$css[] = file_get_contents($file);
+		$lastmodtime = max($lastmodtime, filemtime($file));
+	}
+}
 // Default charset
 $charset = isset($_GET['charset']) ? $_GET['charset']  : '';
 switch ($charset) {
 	case 'Shift_JIS': break; /* this @charset is for Mozilla's bug */
 	default: $charset ='iso-8859-1';
 }
+
+// Output header
+//$matches = array();
+//if(ini_get('zlib.output_compression') && preg_match('/\b(gzip|deflate)\b/i', $_SERVER['HTTP_ACCEPT_ENCODING'], $matches)) {
+//	header('Content-Encoding: ' . $matches[1]);
+//	header('Vary: Accept-Encoding');
+//}
+header('Content-Type: text/css; charset=' . $charset);
+header('Last-Modified: ' . date('r', $lastmodtime));
+
+// Output body 
 echo '@charset "' . $charset . '";' . "\n";
-
-echo file_get_contents('pukiwiki.css');
-echo file_get_contents('plus.css');
-
-$dir = @opendir('./css/');
-while ($file = @readdir($dir)) {
-	$pathparts = pathinfo($file);
-	if (!strcasecmp($path_parts['extension'], 'css')) {
-		echo file_get_contents($file);
-	}
-}
+echo join("\n\n", $css) . "\n\n";
 echo '@media print {'."\n";
 echo file_get_contents('print.css');
 echo '}';
-
 ?>
