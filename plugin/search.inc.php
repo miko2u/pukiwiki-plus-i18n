c7c9597cc61c30ea1380ef644588d0d593e73d14
miko2u <miko@cafelounge.net>
2005-12-20 07:05:23 +0000

    merge official-cvs(2005-12-18) #4 with gettext

--- plugin/search.inc.php
+++ plugin/search.inc.php
@@ -1,6 +1,6 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone.
-// $Id: search.inc.php,v 1.8.3 2005/04/02 06:33:39 miko Exp $
+// $Id: search.inc.php,v 1.13.3 2005/11/29 18:19:51 miko Exp $
 //
 // Search plugin
 
@@ -9,19 +9,19 @@
 define('PLUGIN_SEARCH_DISABLE_GET_ACCESS', 0); // 1, 0
 
 define('PLUGIN_SEARCH_MAX_LENGTH', 80);
+define('PLUGIN_SEARCH_MAX_BASE',   16); // #search(1,2,3,...,15,16)
 
 // Show a search box on a page
 function plugin_search_convert()
 {
 	static $done;
 
-	if (func_get_args()) {
-		return '#search(): No argument<br />' . "\n";
-	} else if (isset($done)) {
+	if (isset($done)) {
 		return '#search(): You already view a search box<br />' . "\n";
 	} else {
 		$done = TRUE;
-		return plugin_search_search_form();
+		$args = func_get_args();
+		return plugin_search_search_form('', '', $args);
 	}
 }
 
@@ -43,11 +43,12 @@ function plugin_search_action()
 	}
 
 	$type = isset($vars['type']) ? $vars['type'] : '';
+	$base = isset($vars['base']) ? $vars['base'] : '';
 
 	if ($s_word != '') {
 		// Search
 		$msg  = str_replace('$1', $s_word, $_title_result);
-		$body = do_search($vars['word'], $type);
+		$body = do_search($vars['word'], $type, FALSE, $base);
 	} else {
 		// Init
 		unset($vars['word']); // Stop using $_msg_word at lib/html.php
@@ -56,23 +57,53 @@ function plugin_search_action()
 	}
 
 	// Show search form
-	$body .= plugin_search_search_form($s_word, $type);
+	$bases = ($base == '') ? array() : array($base);
+	$body .= plugin_search_search_form($s_word, $type, $bases);
 
 	return array('msg'=>$msg, 'body'=>$body);
 }
 
-function plugin_search_search_form($s_word = '', $type = '')
+function plugin_search_search_form($s_word = '', $type = '', $bases = array())
 {
 	global $script;
 	$_btn_search    = _('Search');
 	$_btn_and       = _('AND');
 	$_btn_or        = _('OR');
+	$_search_pages  = _('Search for page starts from $1');
+	$_search_all    = _('Search for all pages');
 
 	$and_check = $or_check = '';
 	if ($type == 'OR') {
 		$or_check  = ' checked="checked"';
 	} else {
 		$and_check = ' checked="checked"';
+	}
+
+	$base_option = '';
+	if (!empty($bases)) {
+		$base_msg = '';
+		$_num = 0;
+		$check = ' checked="checked"';
+		foreach($bases as $base) {
+			++$_num;
+			if (PLUGIN_SEARCH_MAX_BASE < $_num) break;
+			$label_id = '_p_search_base_id_' . $_num;
+			$s_base   = htmlspecialchars($base);
+			$base_str = '<strong>' . $s_base . '</strong>';
+			$base_label = str_replace('$1', $base_str, $_search_pages);
+			$base_msg  .=<<<EOD
+ <div>
+  <input type="radio" name="base" id="$label_id" value="$s_base" $check />
+  <label for="$label_id">$base_label</label>
+ </div>
+EOD;
+			$check = '';
+		}
+		$base_msg .=<<<EOD
+  <input type="radio" name="base" id="_p_search_base_id_all" value="" />
+  <label for="_p_search_base_id_all">$_search_all</label>
+EOD;
+		$base_option = '<div class="small">' . $base_msg . '</div>';
 	}
 
 	if (! PLUGIN_SEARCH_DISABLE_GET_ACCESS) {
@@ -87,6 +118,7 @@ function plugin_search_search_form($s_word = '', $type = '')
   <label for="_p_search_OR">$_btn_or</label>
   &nbsp;<input type="submit" value="$_btn_search" />
  </div>
+$base_option
 </form>
 EOD;
 	}
@@ -100,6 +132,7 @@ EOD;
   <label for="_p_search_OR">$_btn_or</label>
   &nbsp;<input type="submit" value="$_btn_search" />
  </div>
+$base_option
 </form>
 EOD;
 }
