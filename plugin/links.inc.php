a6019c4f2aad8d0ec866cef4e9defd282b2f2039
miko2u <miko@cafelounge.net>
2006-01-16 17:20:55 +0000

    For BugTrack/62 (links).

--- plugin/links.inc.php
+++ plugin/links.inc.php
@@ -1,6 +1,6 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone
-// $Id: links.inc.php,v 1.23.1 2006/01/11 23:30:00 upk Exp $
+// $Id: links.inc.php,v 1.23.2 2006/01/17 01:10:00 upk Exp $
 //
 // Update link cache plugin
 
@@ -13,16 +13,17 @@ function plugin_links_init()
 			'msg_adminpass' => _("Administrator password"),
 			'btn_submit'    => _("Exec"),
 			'msg_done'      => _("The update of cash was completed."),
-			'msg_usage'     => 
+			'msg_usage1'	=> 
 _("* Content of processing\n") .
 _(":Cash update|\n") .
 _("All pages are scanned, whether on which page certain pages have been linked is investigated, and it records in the cache.\n\n") .
 _("* CAUTION\n") .
 _("It is likely to drive it for a few minutes in execution.") .
-_("Please wait for a while after pushing the execution button.\n\n") .
+_("Please wait for a while after pushing the execution button.\n\n"),
+			'msg_usage2'	=> 
 _("* EXEC\n") .
 _("Please input the Administrator password, and click the [Exec] button.\n")
-		)
+		),
 	);
 	set_plugin_messages($messages);
 }
@@ -33,33 +34,39 @@ function plugin_links_action()
 	global $_links_messages;
 
 	// if (PKWK_READONLY) die_message('PKWK_READONLY prohibits this');
-	if (auth::check_role('readonly')) die_message('PKWK_READONLY prohibits this');
+	if (auth::check_role('readonly')) die_message( _("PKWK_READONLY prohibits this") );
 
-	$msg = $body = '';
-	if (empty($vars['action']) || empty($post['adminpass']) || ! pkwk_login($post['adminpass'])) {
-		$msg   = & $_links_messages['title_update'];
-		$body  = convert_html( sprintf($_links_messages['msg_usage']) );
-		$body .= <<<EOD
+	$admin_pass = (empty($post['adminpass'])) ? '' : $post['adminpass'];
+	if ( isset($vars['menu']) && (! auth::check_role('role_adm_contents') || pkwk_login($admin_pass) )) {
+		set_time_limit(0);
+		links_init();
+		$foot_explain = array(); // Exhaust footnotes
+		$msg  = & $_links_messages['title_update'];
+		$body = & $_links_messages['msg_done'    ];
+		return array('msg'=>$msg, 'body'=>$body);
+	}
+
+	$msg   = & $_links_messages['title_update'];
+	$body  = convert_html( sprintf($_links_messages['msg_usage1']) );
+	$body .= <<<EOD
 <form method="POST" action="$script">
  <div>
   <input type="hidden" name="plugin" value="links" />
-  <input type="hidden" name="action" value="update" />
+  <input type="hidden" name="menu" value="1" />
+EOD;
+	if (auth::check_role('role_adm_contents')) {
+		$body .= convert_html( sprintf($_links_messages['msg_usage2']) );
+		$body .= <<<EOD
   <label for="_p_links_adminpass">{$_links_messages['msg_adminpass']}</label>
   <input type="password" name="adminpass" id="_p_links_adminpass" size="20" value="" />
+EOD;
+	}
+	$body .= <<<EOD
   <input type="submit" value="{$_links_messages['btn_submit']}" />
  </div>
 </form>
 EOD;
 
-	} else if ($vars['action'] == 'update') {
-		links_init();
-		$foot_explain = array(); // Exhaust footnotes
-		$msg  = & $_links_messages['title_update'];
-		$body = & $_links_messages['msg_done'    ];
-	} else {
-		$msg  = & $_links_messages['title_update'];
-		$body = & $_links_messages['err_invalid' ];
-	}
 	return array('msg'=>$msg, 'body'=>$body);
 }
 ?>
