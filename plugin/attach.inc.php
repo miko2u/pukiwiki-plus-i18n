80562216087b2217a5ac7911b2fd01398cefd6da
miko2u <miko@cafelounge.net>
2006-02-19 08:50:38 +0000

    Fine-tuning of attach.inc.php #2.

--- plugin/attach.inc.php
+++ plugin/attach.inc.php
@@ -1,6 +1,6 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone
-// $Id: attach.inc.php,v 1.80.13 2006/02/19 19:28:00 upk Exp $
+// $Id: attach.inc.php,v 1.80.14 2006/02/19 17:49:00 upk Exp $
 // Copyright (C)
 //   2005-2006 PukiWiki Plus! Team
 //   2003-2005 PukiWiki Developers Team
@@ -15,16 +15,17 @@
 //    This feature is disabled at newer version of PHP.
 //    Set this at php.ini if you want.
 // Max file size for upload on PHP (PHP default: 2MB)
-ini_set('upload_max_filesize', '4M');
+ini_set('upload_max_filesize', '20M');
 
 // Max file size for upload on script of PukiWikiX_FILESIZE
-define('PLUGIN_ATTACH_MAX_FILESIZE', (2048 * 1024)); // default: 1MB
+define('PLUGIN_ATTACH_MAX_FILESIZE', (2048 * 1024 * 20)); // default: 1MB
 
 // 管理者だけが添付ファイルをアップロードできるようにする
 define('PLUGIN_ATTACH_UPLOAD_ADMIN_ONLY', FALSE); // FALSE or TRUE
 
 // 管理者だけが添付ファイルを削除できるようにする
-define('PLUGIN_ATTACH_DELETE_ADMIN_ONLY', FALSE); // FALSE or TRUE
+//define('PLUGIN_ATTACH_DELETE_ADMIN_ONLY', FALSE); // FALSE or TRUE
+define('PLUGIN_ATTACH_DELETE_ADMIN_ONLY', TRUE); // FALSE or TRUE
 
 // 管理者が添付ファイルを削除するときは、バックアップを作らない
 // PLUGIN_ATTACH_DELETE_ADMIN_ONLY=TRUEのとき有効
@@ -685,38 +686,16 @@ EOD;
  [<a href="$script?plugin=attach&amp;pcmd=list">{$_attach_messages['msg_listall']}</a>]
 </p>
 <dl>
- <dt>$info (<a href="{$this->filename}" title="{$this->filename}">{$_attach_messages['msg_filename']}</a>)</dt>
+ <dt>$info (<a href="#" title="{$this->filename}">{$_attach_messages['msg_filename']}</a>)</dt>
 </dl>
 {$_attach_setimage}
 <dl>
- <table class="style_table" cellspacing="1" border="0">
-  <tbody>
-   <tr>
-    <th class="style_th">{$_attach_messages['msg_page']}</th>
-    <td class="style_td">$s_page</td>
-   </tr>
-   <tr>
-    <th class="style_th">{$_attach_messages['msg_filesize']}</th>
-    <td class="style_td">{$this->size_str} ({$this->size} bytes)</td>
-   </tr>
-   <tr>
-    <th class="style_th">Content-type</th>
-    <td class="style_td">{$this->type}</td>
-   </tr>
-   <tr>
-    <th class="style_th">{$_attach_messages['msg_date']}</th>
-    <td class="style_td">{$this->time_str}</td>
-   </tr>
-   <tr>
-    <th class="style_th">{$_attach_messages['msg_dlcount']}</th>
-    <td class="style_td">{$this->status['count'][$this->age]}</td>
-   </tr>
-   <tr>
-    <th class="style_th">{$_attach_messages['msg_md5hash']}</th>
-    <td class="style_td">{$this->md5hash}</td>
-   </tr>
-  </tbody>
- </table>
+ <dd>{$_attach_messages['msg_page']}:$s_page</dd>
+ <dd>{$_attach_messages['msg_filesize']}:{$this->size_str} ({$this->size} bytes)</dd>
+ <dd>Content-type:{$this->type}</dd>
+ <dd>{$_attach_messages['msg_date']}:{$this->time_str}</dd>
+ <dd>{$_attach_messages['msg_dlcount']}:{$this->status['count'][$this->age]}</dd>
+ <dd>{$_attach_messages['msg_md5hash']}:{$this->md5hash}</dd>
  $msg_freezed
 </dl>
 <div style="clear:right"><hr /></div>
