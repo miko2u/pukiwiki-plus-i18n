d5703231764bfd83a51fbbc213c09b57733ffd01
miko2u <miko@cafelounge.net>
2006-03-24 07:56:01 +0000

    For order at the time of the client #2.

--- plugin/tz.inc.php
+++ plugin/tz.inc.php
@@ -3,14 +3,15 @@
  * tzCalculation_LocalTimeZone Plugin
  *
  * @copyright   Copyright &copy; 2006, Katsumi Saito <katsumi@jo1upk.ymt.prug.or.jp>
- * @version	$Id: tz.inc.php,v 0.1 2006/03/24 01:08:00 upk Exp $
+ * @version	$Id: tz.inc.php,v 0.2 2006/03/24 01:15:00 upk Exp $
  * @license	http://opensource.org/licenses/gpl-license.php GNU Public License
  * @link	http://www.desisoftsystems.com/white-papers/timeZoneCalculation/
  */
 function plugin_tz_convert()
 {
-	global $script;
+	global $script, $use_local_time;
 
+	if ($use_local_time) return '';
 	if (isset($_COOKIE['timezone'])) return '';
 	$url = parse_url($script);
 
