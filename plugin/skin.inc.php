252ec0fb4d35bdbfd5c3219323d3165c621e0778
miko2u <miko@cafelounge.net>
2006-04-25 17:27:56 +0000

    For DATA_HOME.

--- plugin/skin.inc.php
+++ plugin/skin.inc.php
@@ -2,7 +2,7 @@
 /////////////////////////////////////////////////
 // PukiWiki - Yet another WikiWikiWeb clone.
 //
-// $Id: skin.inc.php,v 1.1.3 2004/12/27 23:38:46 miko Exp $
+// $Id: skin.inc.php,v 1.1.4 2006/04/26 02:23:00 upk Exp $
 //
 function plugin_skin_convert()
 {
@@ -13,17 +13,19 @@ function plugin_skin_convert()
 	}
 	list($skin_name) = func_get_args();
 
-	$skin_temp = DATA_HOME . SKIN_DIR . basename($skin_name) . '.skin.php';
-	if (file_exists($skin_temp) && is_readable($skin_temp)) {
-		$skin_file = $skin_temp;
-		return '';
-	}
 
-	// Back compat for Pukiwiki Plus!(1.4.4plus-u2)
-	$skin_temp = DATA_HOME . SKIN_DIR . basename($skin_name) . '.skin.' . LANG . '.php';
-	if (file_exists($skin_temp) && is_readable($skin_temp)) {
-		$skin_file = $skin_temp;
-		return '';
+	$skin_temp = array(
+		SKIN_URI . basename($skin_name) . '.skin.php',
+		DATA_HOME . SKIN_DIR . basename($skin_name) . '.skin.php',
+		// Back compat for Pukiwiki Plus!(1.4.4plus-u2)
+		DATA_HOME . SKIN_DIR . basename($skin_name) . '.skin.' . LANG . '.php',
+	);
+
+	foreach($skin_temp as $skin) {
+		if (file_exists($skin) && is_readable($skin)) {
+			$skin_file = $skin;
+			return '';
+		}
 	}
 
 	return '<!-- Cant replace skin. Use default. -->';
