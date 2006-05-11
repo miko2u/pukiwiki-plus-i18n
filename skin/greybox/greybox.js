5c681061445fa9c129b500a684b78f6ea4a42e3b
miko2u <miko@cafelounge.net>
2006-05-11 17:13:50 +0000

    Greybox 1.92 to 1.62.

--- skin/greybox/greybox.js
+++ skin/greybox/greybox.js
@@ -1,12 +1,12 @@
 /****
-Last Modified: 28/04/06 16:23:20
+Last Modified: Sun 26 Feb 2006 09:24:10 PM CET
 
  GreyBox - The pop-up window thingie
    Copyright Amir Salihefendic 2006
  AUTHOR
    4mir Salihefendic (http://amix.dk) - amix@amix.dk
  VERSION
-	 1.92
+	 1.62
  LICENSE
   LGPL (read more in LGPL.txt)
  SITE
@@ -18,8 +18,6 @@ var GB_IFRAME = null;
 var GB_OVERLAY = null;
 var GB_TIMEOUT = null;
 
-var GB_URL;
-
 var GB_HEIGHT = 400;
 var GB_WIDTH = 400;
 
@@ -28,44 +26,46 @@ var GB_caption = null;
 //The url that was visited last
 var GB_last_win_url = null;
 
-function GB_show(caption, url /* optional */, height, width, return_win) {
-  if(height != 'undefined')
-    GB_HEIGHT = height;
-  if(width != 'undefined')
-    GB_WIDTH = width;
+function GB_show(caption, url /* optional */, height, width) {
+  try {
+    if(height != 'undefined')
+      GB_HEIGHT = height;
+    if(width != 'undefined')
+      GB_WIDTH = width;
 
-  initIfNeeded();
-
-  GB_caption.innerHTML = caption;
-
-  GB_setPosition();
-  if(GB_ANIMATION) {
-    positionRightVertically(GB_HEADER, -(GB_HEIGHT));
-    positionRightVertically(GB_WINDOW, -(GB_HEIGHT+22));
-  }
-
-  AJS.showElement(GB_OVERLAY);
-  AJS.showElement(GB_HEADER);
-  AJS.showElement(GB_WINDOW);
-
-  GB_setWidth();
-
-  if(GB_ANIMATION) {
-    GB_animateOut(-GB_HEIGHT);
-    GB_URL = url;
-  }
-  else {
+    initIfNeeded();
     GB_IFRAME.src = url;
     GB_IFRAME.opener = this;
+
+    GB_caption.innerHTML = caption;
+
+    GB_setPosition();
+    if(GB_ANIMATION) {
+      positionRightVertically(GB_HEADER, -(GB_HEIGHT));
+      positionRightVertically(GB_WINDOW, -(GB_HEIGHT+22));
+    }
+
+    showElement(GB_OVERLAY);
+    showElement(GB_HEADER);
+    showElement(GB_WINDOW);
+
+    GB_setWidth();
+
+    if(GB_ANIMATION) {
+      GB_animateOut(-GB_HEIGHT);
+    }
+    return false;
   }
-  return false;
+  catch (e) {
+    return false;
+  }
 }
 
 function GB_hide() {
-  GB_IFRAME.src = GB_IMG_DIR + "blank.html";
-  AJS.hideElement(GB_WINDOW);
-  AJS.hideElement(GB_HEADER);
-  AJS.hideElement(GB_OVERLAY);
+  GB_IFRAME.src = "";
+  hideElement(GB_WINDOW);
+  hideElement(GB_HEADER);
+  hideElement(GB_OVERLAY);
 }
 
 function GB_setPosition() {
@@ -80,8 +80,6 @@ function GB_animateOut(top) {
     GB_TIMEOUT = window.setTimeout(function() { GB_animateOut(top+50); }, 1);
   }
   else {
-    GB_IFRAME.src = GB_URL;
-    GB_IFRAME.opener = this;
     GB_WINDOW.style.top = getScrollTop()+22+"px";
     GB_HEADER.style.top = getScrollTop()+"px";
     clearTimeout(GB_TIMEOUT);
@@ -99,10 +97,7 @@ function GB_setWidth() {
   GB_WINDOW.style.height = GB_HEIGHT + "px";
   GB_IFRAME.style.height = GB_HEIGHT - 5 + "px";
 
-  if((navigator.userAgent.toLowerCase().indexOf("firefox") != -1))
-    GB_OVERLAY.style.width = "100%";
-  else
-    GB_OVERLAY.style.width = array_page_size[0] + "px";
+  GB_OVERLAY.style.width = array_page_size[0] + "px";
 
   var max_height = Math.max(getScrollTop()+array_page_size[1], getScrollTop()+GB_HEIGHT+30);
   GB_OVERLAY.style.height = max_height + "px";
@@ -114,33 +109,25 @@ function GB_setWidth() {
 
 function GB_init() {
   //Create the overlay
-  GB_OVERLAY = AJS.DIV({'id': 'GB_overlay'});
-  if(AJS.isIe()) {
-    GB_OVERLAY.style.backgroundColor = "#000000";
-    GB_OVERLAY.style.backgroundColor = "transparent";
-    GB_OVERLAY.style.backgroundImage = "url("+ GB_IMG_DIR +"blank.gif)";
-    GB_OVERLAY.runtimeStyle.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + GB_IMG_DIR + "overlay.png',sizingMethod='scale')";
-  }
-  else 
-    GB_OVERLAY.style.backgroundImage = "url("+ GB_IMG_DIR +"overlay.png)";
+  GB_OVERLAY = DIV({'id': 'GB_overlay'});
 
   if(GB_overlay_click_close)
     GB_OVERLAY.onclick = GB_hide;
 
-  AJS.getBody().insertBefore(GB_OVERLAY, AJS.getBody().firstChild);
+  getBody().insertBefore(GB_OVERLAY, getBody().firstChild);
 
   //Create the window
-  GB_WINDOW = AJS.DIV({'id': 'GB_window'});
+  GB_WINDOW = DIV({'id': 'GB_window'});
 
-  GB_HEADER = AJS.DIV({'id': 'GB_header'});
-  GB_caption = AJS.DIV({'id': 'GB_caption'}, "");
+  GB_HEADER = DIV({'id': 'GB_header'});
+  GB_caption = DIV({'id': 'GB_caption'}, "");
 
-  var close = AJS.DIV({'id': 'GB_close'}, AJS.IMG({'src': GB_IMG_DIR + 'close.gif', 'alt': 'Close window'}));
+  var close = DIV({'id': 'GB_close'}, IMG({'src': GB_IMG_DIR + 'close.gif', 'alt': 'Close window'}));
   close.onclick = GB_hide;
-  AJS.appendChildNodes(GB_HEADER, close, GB_caption);
+  ACN(GB_HEADER, close, GB_caption);
 
-  AJS.getBody().insertBefore(GB_WINDOW, GB_OVERLAY.nextSibling);
-  AJS.getBody().insertBefore(GB_HEADER, GB_OVERLAY.nextSibling);
+  getBody().insertBefore(GB_WINDOW, GB_OVERLAY.nextSibling);
+  getBody().insertBefore(GB_HEADER, GB_OVERLAY.nextSibling);
 
 }
 
@@ -151,10 +138,10 @@ function initIfNeeded() {
     window.onscroll = function() { GB_setPosition(); GB_setWidth(); };
   } 
   //Remove the old iFrame
-  var new_frame = AJS.IFRAME({'id': 'GB_frame', 'name': 'GB_frame'});
+  var new_frame = IFRAME({'id': 'GB_frame', 'name': 'GB_frame'});
   if (GB_IFRAME != null)
-    AJS.removeElement(GB_IFRAME);
-  AJS.appendChildNodes(GB_WINDOW, new_frame);
+    removeElement(GB_IFRAME);
+  ACN(GB_WINDOW, new_frame);
   GB_IFRAME = new_frame;
 }
 
