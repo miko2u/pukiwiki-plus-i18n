6686ca5b9feaa2df9f59cdb4d5418231873b5c6b
miko2u <miko@cafelounge.net>
2006-06-22 05:46:00 +0000

    For v3.1 of greybox.inc.php.

--- plugin/greybox.inc.php
+++ plugin/greybox.inc.php
@@ -3,8 +3,9 @@
  * GreyBox プラグイン
  *
  * @copyright   Copyright &copy; 2006, Katsumi Saito <katsumi@jo1upk.ymt.prug.or.jp>
- * @version     $Id: greybox.inc.php,v 0.5 2006/05/03 21:06:00 upk Exp $
+ * @version     $Id: greybox.inc.php,v 0.6 2006/06/22 14:33:00 upk Exp $
  * @link	http://amix.dk/projects/?page_id=5
+ *              http://orangoo.com/labs/GreyBox/
  */
 
 function plugin_greybox_convert()
@@ -15,6 +16,7 @@ function plugin_greybox_convert()
 	$argc = func_num_args();
 
 	// <a href="#" onclick="return GB_show('caption', 'url', height, width);">caption</a>
+	// <a href="#" onclick="return GB_showFullScreen('caption', 'url');">caption</a>
 	$field = array('caption','url','img','height', 'width');
 	for($i=0; $i<$argc; $i++) {
 		$$field[$i] = htmlspecialchars($argv[$i], ENT_QUOTES);
@@ -23,8 +25,16 @@ function plugin_greybox_convert()
 	if (empty($url)) return 'usage: #greybox(caption, url, img, height, width)';
 	if (empty($caption)) $caption = 'no title';
 	if (empty($img)) $img = '';
-	if (empty($height)) $height = 470;
-	if (empty($width)) $width = 600;
+
+	$is_full = FALSE;
+	if (empty($height)) {
+		$is_full = TRUE;
+		$height = 470;
+	}
+	if (empty($width)) {
+		$is_full = TRUE;
+		$width = 600;
+	}
 
 	if (! empty($img)) {
 		// <img src="" alt="" title="" width="127" height="38" />
@@ -34,7 +44,11 @@ function plugin_greybox_convert()
 	}
 
 	$caption2 = str_replace('&amp;#039;','\'',$caption2); // ' の対応
-	return '<a href="#" onclick="return GB_show(\''.$caption.'\', \''.$url.'\', '.$height.', '.$width.');">'.$caption2."</a>\n";
+	if ($is_full) {
+		return '<a href="#" onclick="return GB_showFullScreen(\''.$caption.'\', \''.$url.'\');">'.$caption2."</a>\n";
+	} else {
+		return '<a href="#" onclick="return GB_show(\''.$caption.'\', \''.$url.'\', '.$height.', '.$width.');">'.$caption2."</a>\n";
+	}
 }
 
 function plugin_greybox_inline()
