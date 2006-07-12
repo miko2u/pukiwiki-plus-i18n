1b7523b23d8f65f0a302636d7d6dcfe086efd05c
miko2u <miko@cafelounge.net>
2006-07-12 08:39:42 +0000

    Cleanup

--- plugin/aname.inc.php
+++ plugin/aname.inc.php
@@ -105,12 +105,15 @@ function plugin_aname_tag($args = array(), $convert = TRUE)
 	$class   = $f_super ? 'anchor_super' : 'anchor';
 //miko
 	// Moblie Phone is not xhtml. umm...
-	if (isset($pkwk_dtd) && $pkwk_dtd < PKWK_DTD_XHTML_1_1) {
-		$attr_id = $f_noid  ? '' : ' id="' . $id . '" name ="' . $id . '"';
+	if ($f_noid) {
+		$attr_id = '';
+	} elseif (isset($pkwk_dtd) && $pkwk_dtd < PKWK_DTD_XHTML_1_1) {
+		$attr_id = ' id="' . $id . '" name="' . $id . '"';
 	} elseif (!defined('UA_PROFILE') || UA_PROFILE == 'default') {
-		$attr_id = $f_noid  ? '' : ' id="' . $id . '"';
+		$attr_id = ' id="' . $id . '"';
 	} else {
-		$attr_id = $f_noid  ? '' : ' id="' . $id . '" name ="' . $id . '"';
+		// Force XHTML1/HTML4
+		$attr_id = ' id="' . $id . '" name="' . $id . '"';
 	}
 //miko
 	$url     = $f_full  ? get_script_uri() . '?' . rawurlencode($vars['page']) : '';
