2e9aa4b6aa9b5d842a12fcce11c13813b6d90d86
miko2u <miko@cafelounge.net>
2006-08-09 11:19:19 +0000

    For BugTrack/90.

--- skin/lang/en_US.js
+++ skin/lang/en_US.js
@@ -1,7 +1,7 @@
 /////////////////////////////////////////////////
 // PukiWiki Plus!
 //
-// $Id: en_US.js,v 1.0.0 2006/02/13 02:34:00 upk Exp $
+// $Id: en_US.js,v 1.0.1 2006/08/09 20:13:00 upk Exp $
 //
 
 // Set masseges.
@@ -9,11 +9,11 @@ var pukiwiki_msg_copyed = "It copied it onto the clipboard.";
 var pukiwiki_msg_select = "Please select the range of the object.";
 var pukiwiki_msg_fontsize = "Size of character ( It specifies it with % or pt[omit]. ): ";
 var pukiwiki_msg_to_ncr = "It converts it into the numeric character reference.";
-var pukiwiki_msg_hint = '<img src="image/plus/hint.png" width="18" height="16" border="0" title="hint" alt="hint" />';
+var pukiwiki_msg_hint = '<img src="'+IMAGE_DIR+'plus/hint.png" width="18" height="16" border="0" title="hint" alt="hint" />';
 var pukiwiki_msg_winie_hint_text = "\n\nThe color selected first becomes the color specification and the character color and the color selected next become the background colors.\n\nAfter processing the range of the selection, the range remains selecting it.\nPlease input it after moving the cursor with [ → ] key when you continuously input the character.\n\n\n-- +α(Advance mode) --\n\n[ &# ] button converts the selection character string into the numeric character reference.";
 var pukiwiki_msg_gecko_hint_text = pukiwiki_msg_winie_hint_text + "\n\n" + "Please push the [ ESC ] key when the range of the display returns to the head, and the processed range disappears.";
-var pukiwiki_msg_to_easy_t = '<img src="image/plus/easy.png" width="18" height="16" border="0" title="easy" alt="easy" />';
-var pukiwiki_msg_to_adv_t = '<img src="image/plus/adv.png" width="18" height="16" border="0" title="adv" alt="adv" />';
+var pukiwiki_msg_to_easy_t = '<img src="'+IMAGE_DIR+'plus/easy.png" width="18" height="16" border="0" title="easy" alt="easy" />';
+var pukiwiki_msg_to_adv_t = '<img src="'+IMAGE_DIR+'plus/adv.png" width="18" height="16" border="0" title="adv" alt="adv" />';
 var pukiwiki_msg_to_easy = "It changed to an easy mode.\nAfter reload, it becomes effective.\n\nIs it right now reload?";
 var pukiwiki_msg_to_adv = "It changed to the advance mode.\nAfter reload, it becomes effective.\n\nIs it right now reload?";
 var pukiwiki_msg_inline1 = "Please input the plugin name. [ & is omitted ]";
--- skin/lang/ja_JP.js
+++ skin/lang/ja_JP.js
@@ -1,7 +1,7 @@
 /////////////////////////////////////////////////
 // PukiWiki Plus!
 //
-// $Id: ja_JP.js,v 1.0.0 2006/02/13 02:34:00 upk Exp $
+// $Id: ja_JP.js,v 1.0.1 2006/08/09 20:12:00 upk Exp $
 //
 
 // Set masseges.
@@ -9,11 +9,11 @@ var pukiwiki_msg_copyed = "クリップボードにコピーしました。";
 var pukiwiki_msg_select = "対象範囲を選択してください。";
 var pukiwiki_msg_fontsize = "文字の大きさ ( % または pt[省略可] で指定): ";
 var pukiwiki_msg_to_ncr = "数値文字参照へ変換";
-var pukiwiki_msg_hint = '<img src="image/plus/hint.png" width="18" height="16" border="0" title="hint" alt="hint" />';
+var pukiwiki_msg_hint = '<img src="'+IMAGE_DIR+'plus/hint.png" width="18" height="16" border="0" title="hint" alt="hint" />';
 var pukiwiki_msg_winie_hint_text = "\n\n色指定は、最初に選択した色が文字色、次に選択した色が背景色になります。\n\n選択範囲を処理後は、その範囲が選択したままになっています。\n続けて文字を入力する場合は、[ → ]キーでカーソルを移動してから入力してください。\n\n\n-- +α(アドバンスモード) --\n\n[ &# ] ボタンは、選択文字列を数値文字参照に変換します。";
 var pukiwiki_msg_gecko_hint_text = pukiwiki_msg_winie_hint_text + "\n\n" + "表示範囲が先頭に戻ってしまい、処理した範囲が見えなくなった時は、[ ESC ]キーを押してみてください。";
-var pukiwiki_msg_to_easy_t = '<img src="image/plus/easy.png" width="18" height="16" border="0" title="easy" alt="easy" />';
-var pukiwiki_msg_to_adv_t = '<img src="image/plus/adv.png" width="18" height="16" border="0" title="adv" alt="adv" />';
+var pukiwiki_msg_to_easy_t = '<img src="'+IMAGE_DIR+'plus/easy.png" width="18" height="16" border="0" title="easy" alt="easy" />';
+var pukiwiki_msg_to_adv_t = '<img src="'+IMAGE_DIR+'plus/adv.png" width="18" height="16" border="0" title="adv" alt="adv" />';
 var pukiwiki_msg_to_easy = "イージーモードに変更しました。\nリロード後に有効になります。\n\n今すぐリロードしますか？";
 var pukiwiki_msg_to_adv = "アドバンスモードに変更しました。\nリロード後に有効になります。\n\n今すぐリロードしますか？";
 var pukiwiki_msg_inline1 = "プラグイン名を入力してください。[ ＆ は省く ]";
