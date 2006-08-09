54a9d07c2b7fcd61590201aad50c47188f55ddfe
miko2u <miko@cafelounge.net>
2006-08-09 11:45:47 +0000

    For BugTrack/90.

--- skin/lang/zh_CN.js
+++ skin/lang/zh_CN.js
@@ -1,25 +1,25 @@
 /////////////////////////////////////////////////
 // PukiWiki Plus!
 //
-// $Id: zh_CN.js,v 1.0.1 2006/08/09 20:17:00 upk Exp $
+// $Id: zh_CN.js,v 1.0.1 2006/08/09 20:44:00 upk Exp $
 //
 
 // Set masseges.
-var pukiwiki_msg_copyed = "剪?板?制。";
-var pukiwiki_msg_select = "????象范?。";
+var pukiwiki_msg_copyed = "剪贴板复制。";
+var pukiwiki_msg_select = "请选择对象范围。";
 var pukiwiki_msg_fontsize = "文字的大小 ( % 又 pt[省略可]指定): ";
-var pukiwiki_msg_to_ncr = "??向数?文字参照";
+var pukiwiki_msg_to_ncr = "变换向数值文字参照";
 var pukiwiki_msg_hint = '<img src="'+IMAGE_DIR+'plus/hint.png" width="18" height="16" border="0" title="hint" alt="hint" />';
-var pukiwiki_msg_winie_hint_text = "\n\n?色指定，在最初??成???了的?色文字?色，其次??了的?色背景?色。\n\n?理后，那个范???了的着?成??范?。\n如果???入文字用、[→]?匙移?光?之后??入。\n\n\n-- +α(??方式) --\n\n[ &# ] 按?，数?文字参照????字符串。";
-var pukiwiki_msg_gecko_hint_text = pukiwiki_msg_winie_hint_text + "\n\n" + "表示范?前?回来了，?理了的范?看不?了的?候，?[?着按ESC ]?匙。";
+var pukiwiki_msg_winie_hint_text = "\n\n颜色指定，在最初时变成为选择了的颜色文字颜色，其次选择了的颜色背景颜色。\n\n处理后，那个范围选择了的着变成选择范围。\n如果继续输入文字用、[→]钥匙移动光标之后请输入。\n\n\n-- +α(扩张方式) --\n\n[ &# ] 按钮，数值文字参照转换选择字符串。";
+var pukiwiki_msg_gecko_hint_text = pukiwiki_msg_winie_hint_text + "\n\n" + "表示范围前头回来了，处理了的范围看不见了的时候，请[试着按ESC ]钥匙。";
 var pukiwiki_msg_to_easy_t = '<img src="'+IMAGE_DIR+'plus/easy.png" width="18" height="16" border="0" title="easy" alt="easy" />';
 var pukiwiki_msg_to_adv_t = '<img src="'+IMAGE_DIR+'plus/adv.png" width="18" height="16" border="0" title="adv" alt="adv" />';
-var pukiwiki_msg_to_easy = "?更了???方式。\n?再?包含在内后?得有效。\n\n?在?上再?包含在内做?？";
-var pukiwiki_msg_to_adv = "?更了???方式。\n?再?包含在内后?得有效。\n\n?在?上再?包含在内做?？";
-var pukiwiki_msg_inline1 = "??入插件名。[ ＆ 省却 ]";
-var pukiwiki_msg_inline2 = "??入参数。[ ( )内? ]";
-var pukiwiki_msg_inline3 = "??入本文。[ { }内? ]";
-var pukiwiki_msg_link = "??入?定?接的文字。";
-var pukiwiki_msg_url = "??入?接?的URL。";
-var pukiwiki_msg_elem = "???做?理的?象。";
+var pukiwiki_msg_to_easy = "变更了为简单方式。\n对再读包含在内后变得有效。\n\n现在马上再读包含在内做吗？";
+var pukiwiki_msg_to_adv = "变更了为扩张方式。\n对再读包含在内后变得有效。\n\n现在马上再读包含在内做吗？";
+var pukiwiki_msg_inline1 = "请输入插件名。[ ＆ 省却 ]";
+var pukiwiki_msg_inline2 = "请输入参数。[ ( )内侧 ]";
+var pukiwiki_msg_inline3 = "请输入本文。[ { }内侧 ]";
+var pukiwiki_msg_link = "请输入设定链接的文字。";
+var pukiwiki_msg_url = "请输入链接处的URL。";
+var pukiwiki_msg_elem = "请选择做处理的对象。";
 
