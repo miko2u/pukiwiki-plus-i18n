b86628b7138b0a94973e2e3fa3807ab8754ddb99
miko2u <miko@cafelounge.net>
2006-09-12 05:33:54 +0000

    fix include check "sidebar" page

--- plugin/include.inc.php
+++ plugin/include.inc.php
@@ -52,7 +52,7 @@ define('PLUGIN_INCLUDE_USAGE', '#include(): Usage: (a-page-name-you-want-to-incl
 
 function plugin_include_convert()
 {
-	global $script, $vars, $get, $post, $menubar, $_msg_include_restrict;
+	global $script, $vars, $get, $post, $menubar, $sidebar, $_msg_include_restrict;
 	static $included = array();
 	static $count = 1;
 
