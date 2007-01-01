f7b88422f9f6d90df8210b07d254c92db5fdedf7
miko2u <miko@cafelounge.net>
2007-01-01 16:17:01 +0000

    fix bug.

--- plugin/pagebreak.inc.php
+++ plugin/pagebreak.inc.php
@@ -3,12 +3,12 @@
  * PageBreak Plugin
  *
  * @copyright   Copyright &copy; 2007, Katsumi Saito <katsumi@jo1upk.ymt.prug.or.jp>
- * @version     $Id: pagebreak.inc.php,v 0.2 2007/01/02 01:12:00 upk Exp $
+ * @version     $Id: pagebreak.inc.php,v 0.3 2007/01/02 01:16:00 upk Exp $
  * @license     http://opensource.org/licenses/gpl-license.php GNU Public License (GPL2)
  */
 function plugin_pagebreak_convert()
 {
 	// page-break-before, page-break-after, page-break-inside
-	return '<span style="page-break-before: always;"></span>'."\n";
+	return '<div style="page-break-before: always;">&nbsp;</div>'."\n";
 }
 ?>
