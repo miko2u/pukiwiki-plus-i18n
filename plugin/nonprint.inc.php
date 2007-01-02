d434276875b61425b63905c88f358660f3b3d13b
miko2u <miko@cafelounge.net>
2007-01-02 16:13:53 +0000

    Expansion of range

--- plugin/nonprint.inc.php
+++ plugin/nonprint.inc.php
@@ -3,18 +3,21 @@
  * Non Print Plugin
  *
  * @copyright   Copyright &copy; 2007, Katsumi Saito <katsumi@jo1upk.ymt.prug.or.jp>
- * @version     $Id: nonprint.inc.php,v 0.3 2007/01/03 00:08:00 upk Exp $
+ * @version     $Id: nonprint.inc.php,v 0.4 2007/01/03 01:03:00 upk Exp $
  * @license     http://opensource.org/licenses/gpl-license.php GNU Public License (GPL2)
  */
 function plugin_nonprint_convert()
 {
 	global $head_tags;
 	static $key = array(
+		'header'		=> 'div#header',
+		'footer'		=> 'div#footer',
 		'title'			=> 'h1.title',
+		'span'			=> 'span.small',
+		'hr'			=> 'hr.full_hr',
 		'topicpath'		=> 'div#topicpath',
 		'calstick_calendar'	=> 'table.calstick_calendar',
 		'sbm'			=> '.sbm',
-
 	);
 
 	$argv = func_get_args();
