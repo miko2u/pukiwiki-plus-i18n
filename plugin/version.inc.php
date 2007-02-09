caa43e63a1902e54ebad8d667c15c83df6045b29
miko2u <miko@cafelounge.net>
2007-02-09 04:55:06 +0000

    Merge official cvs(2006-02-07) for i18n #2

--- plugin/version.inc.php
+++ plugin/version.inc.php
@@ -1,22 +1,27 @@
 <?php
 // PukiWiki - Yet another WikiWikiWeb clone
-// $Id: version.inc.php,v 1.8.1 2006/01/11 23:59:00 upk Exp $
+// $Id: version.inc.php,v 1.9.1 2007/01/21 14:25:25 miko Exp $
+// Copyright (C)
+//   2005-2007 PukiWiki Plus! Team
+//   2002-2003, 2005, 2007 PukiWiki Developers Team
+// License: GPL v2 or (at your option) any later version
 //
-// Show PukiWiki version
+// Show PukiWiki version (maybe a sample code)
+
+function plugin_version_value()
+{
+//	if (PKWK_SAFE_MODE) return '';
+	if (auth::check_role('safemode')) return '';
+	return S_VERSION;
+}
 
 function plugin_version_convert()
 {
-	// if (PKWK_SAFE_MODE) return ''; // Show nothing
-	if (auth::check_role('safemode')) return ''; // Show nothing
-
-	return '<p>' . S_VERSION . '</p>';
+	return '<p>' . plugin_version_value() . '</p>';
 }
 
 function plugin_version_inline()
 {
-	// if (PKWK_SAFE_MODE) return ''; // Show nothing
-	if (auth::check_role('safemode')) return ''; // Show nothing
-
-	return S_VERSION;
+	return plugin_version_value();
 }
 ?>
