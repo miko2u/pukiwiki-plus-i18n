c993f0eafe323d808f82026e10a1a4c6df9a8e20
miko2u <miko@cafelounge.net>
2007-04-22 17:49:27 +0000

    Addition of plugin.

--- plugin/nonlist.inc.php
+++ plugin/nonlist.inc.php
@@ -8,30 +8,36 @@ function plugin_nonlist_action()
 	$_title_nonlist = _('List of non_list pages');
 
 	if (auth::check_role('role_adm_contents')) return '';
-	$env = (isset($vars['env'])) ? 1 : 0;
-	$col = (isset($vars['col'])) ? 1 : 0;
+
+	if (isset($vars['env'])) {
+		$cmd = 2;
+	} elseif (isset($vars['col'])) {
+		$cmd = 1;
+	} else {
+		$cmd = 0;
+	}
 
 	return array(
 		'msg'=> $_title_nonlist,
-		'body'=>plugin_nonlist_getlist($col,$env));
+		'body'=>plugin_nonlist_getlist($cmd));
 }
 
-function plugin_nonlist_getlist($col=0,$env=0)
+function plugin_nonlist_getlist($cmd=0)
 {
 	global $non_list, $whatsnew;
 
-        $pages = array_diff(auth::get_existpages(),array($whatsnew));
-	$pages = preg_grep('/' . $non_list . '/S', $pages);
-
-	if (empty($pages)) return '';
-	if (!$col && !$env) {
+	if ($cmd == 0) {
+        	$pages = array_diff(auth::get_existpages(),array($whatsnew));
+		$pages = preg_grep('/' . $non_list . '/S', $pages);
+		if (empty($pages)) return '';
 		return page_list($pages,'read',false);
 	}
 
+	$pages = array_diff(auth::get_existpages(),array($whatsnew));
 	$tmp = array();
 	foreach($pages as $file=>$page) {
-		if ($col && substr($page,0,1) !== ':') continue;
-		if ($env && substr($page,0,8) !== ':config/') continue;
+		if (substr($page,0,1) !== ':') continue;
+		if ($cmd == 2 && substr($page,0,8) !== ':config/') continue;
 		$tmp[$file] = $page;
 	}
 
