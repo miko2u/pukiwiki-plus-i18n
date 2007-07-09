4f154c93b17cf262f58ab5bd1f20a61f02e65e4e
miko2u <miko@cafelounge.net>
2007-07-09 15:54:43 +0000

    For BugTrack/138 #3.

--- plugin/js_init.inc.php
+++ plugin/js_init.inc.php
@@ -3,7 +3,7 @@
  * js_init - JavaScript 初期化プラグイン
  *
  * @copyright   Copyright &copy; 2006-2007, Katsumi Saito <katsumi@jo1upk.ymt.prug.or.jp>
- * @version     $Id: js_init.inc.php,v 0.2 2007/07/09 23:34:00 upk Exp $
+ * @version     $Id: js_init.inc.php,v 0.3 2007/07/10 00:54:00 upk Exp $
  *
  */
 
@@ -13,9 +13,14 @@ function plugin_js_init_convert()
 
 	$rc = '';
 	//$const = array('SKIN_URI','IMAGE_URI','DEFAULT_LANG');
-	$const = array('SKIN_DIR','IMAGE_DIR','DEFAULT_LANG');
-	foreach( $const as $var){
-		$rc .= 'var '.$var.'="'.constant($var).'";'."\n";
+	//$const = array('SKIN_DIR','IMAGE_DIR','DEFAULT_LANG');
+	//foreach( $const as $var){
+	//	$rc .= 'var '.$var.'="'.constant($var).'";'."\n";
+	//}
+
+	$const = array('SKIN_DIR'=>'SKIN_URI','IMAGE_DIR'=>'IMAGE_URI','DEFAULT_LANG'=>'DEFAULT_LANG');
+	foreach( $const as $key=>$val){
+		$rc .= 'var '.$key.'="'.constant($val).'";'."\n";
 	}
 	unset($const);
 
