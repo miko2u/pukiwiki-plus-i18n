8cab0190c0c68309129378abde269e1e1cb6566c
miko2u <miko@cafelounge.net>
2008-08-30 13:15:01 +0000

    For correction leakage.

--- plugin/tz.inc.php
+++ plugin/tz.inc.php
@@ -2,18 +2,18 @@
 /**
  * tzCalculation_LocalTimeZone Plugin
  *
- * @copyright   Copyright &copy; 2006, Katsumi Saito <katsumi@jo1upk.ymt.prug.or.jp>
- * @version	$Id: tz.inc.php,v 0.2 2006/03/24 01:15:00 upk Exp $
+ * @copyright   Copyright &copy; 2006,2008, Katsumi Saito <katsumi@jo1upk.ymt.prug.or.jp>
+ * @version	$Id: tz.inc.php,v 0.3 2008/08/30 21:38:00 upk Exp $
  * @license	http://opensource.org/licenses/gpl-license.php GNU Public License
  * @link	http://www.desisoftsystems.com/white-papers/timeZoneCalculation/
  */
 function plugin_tz_convert()
 {
-	global $script, $use_local_time;
+	global $use_local_time;
 
 	if ($use_local_time) return '';
 	if (isset($_COOKIE['timezone'])) return '';
-	$url = parse_url($script);
+	$url = parse_url( get_script_absuri() );
 
 	if (empty($url['host'])) return '';
 
